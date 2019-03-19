c2c7e8591bc8c547acea2b0677c396138fe211de
MukaSchultze <samuelschultze@gmail.com>
2019-03-19 20:49:26 -0300

    Fast file enumeration and options

--- dark-skin/Program.cs
+++ dark-skin/Program.cs
@@ -2,13 +2,47 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using CodeProject;
 
 namespace DarkSkin {
     public static class Program {
 
-        private const bool TO_ENABLE = true;
+        private static bool m_toEnable = true;
+        private static bool m_useFastFileEnumerator = false;
+
+        public static ParallelQuery<TSource> Tap<TSource>(this ParallelQuery<TSource> source, Action<TSource> action) {
+            return source.Select(item => {
+                action(item);
+                return item;
+            });
+        }
+
+        public static IEnumerable<TSource> Tap<TSource>(this IEnumerable<TSource> source, Action<TSource> action) {
+            return source.Select(item => {
+                action(item);
+                return item;
+            });
+        }
 
         private static void Main(params string[] args) {
+
+            var toEnable = args.Contains("enable");
+            var toDisable = args.Contains("disable");
+            var help = args.Contains("-h") || args.Contains("--help");
+
+            if (toEnable == toDisable || help) {
+                if (!help)
+                    Console.Error.WriteLine("Invalid parameters");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("  dark-skin.exe enable | disable [options]");
+                Console.WriteLine("");
+                Console.WriteLine("-h, --help             Show this screen");
+                Console.WriteLine("-f, --fast-enumerator  Use fast file enumeration, otherwise use recursive enumeration");
+                return;
+            }
+
+            m_toEnable = toEnable;
+            m_useFastFileEnumerator = args.Contains("-f") || args.Contains("--fast-enumerator");
 
             Console.Title = "Dark Skin for Unity";
 
@@ -26,30 +60,33 @@ namespace DarkSkin {
                     .Select(exe => new UnitySkin(exe, false))
                     .Where(unity => unity.OffsetOfSkinFlags != -1 && unity.SkinIndex != -1)
                     .Where(unity => {
-                        var shouldChange = (TO_ENABLE && unity.IsWhiteSkin) || (!TO_ENABLE && unity.IsDarkSkin);
+                        var shouldChange = (toEnable && unity.IsWhiteSkin) || (!toEnable && unity.IsDarkSkin);
                         if (!shouldChange)
                             unity.Log("Skin already applied, ignoring");
                         return shouldChange;
                     })
-                    .ForAll(unity => unity.SetDarkSkinEnable(TO_ENABLE));
+                    .ForAll(unity => unity.SetDarkSkinEnable(toEnable));
 
             } catch (Exception e) {
-                Console.WriteLine("\nError");
-                Console.WriteLine(e.Message);
-            } finally {
-                Console.WriteLine("\nFinished");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey(true);
+                Console.Error.WriteLine("\nError");
+                Console.Error.WriteLine(e.Message);
             }
-
         }
 
         private static List<string> GetUnityInstallations(string root) {
+
+            if (m_useFastFileEnumerator)
+                return FastDirectoryEnumerator.EnumerateFiles(root, "Unity.exe", SearchOption.AllDirectories)
+                    .AsParallel()
+                    .Tap(file => Console.WriteLine("Found {0}", file.Path))
+                    .Select(file => file.Path)
+                    .ToList();
 
             var folders = new List<string>();
             var unity = Path.Combine(root, "Unity.exe");
 
             if (File.Exists(unity)) {
+                Console.WriteLine("Found {0}", unity);
                 folders.Add(unity);
                 return folders;
             }
--- dark-skin/UnitySkin.cs
+++ dark-skin/UnitySkin.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace DarkSkin {
     public class UnitySkin {
 
-        public static readonly string[] WHITE_HEX = new[] {
+        public static readonly string[] WHITE_HEX = new [] {
             "84 C0 75 08 33 C0 48 83 C4 20 5B C3 8B 03 48 83 C4 20 5B C3", // <= 2018.2
             "84 C0 75 08 33 C0 48 83 C4 30 5B C3 8B 03 48 83 C4 30 5B C3", // == 2018.3 
-            "84 DB 74 04 33 C0 EB 02 8B 07" ,// >= 2019.1
+            "84 DB 74 04 33 C0 EB 02 8B 07", // >= 2019.1
             // "74 56 48 8B 7C 24 30 48 83 7C 24 38 00 77 2F 48 85 FF 74 2A 48 8B 74 24 48 48 8B 0B 48 85 C9 74 10 48 83 7B 08 00 76 09 48 8D 53"
         };
-        public static readonly string[] DARK_HEX = new[] {
+        public static readonly string[] DARK_HEX = new [] {
             "84 C0 74 08 33 C0 48 83 C4 20 5B C3 8B 03 48 83 C4 20 5B C3", // <= 2018.2 
             "84 C0 74 08 33 C0 48 83 C4 30 5B C3 8B 03 48 83 C4 30 5B C3", // == 2018.3 
             "84 DB 75 04 33 C0 EB 02 8B 07", // >= 2019.1
@@ -37,29 +37,28 @@ namespace DarkSkin {
 
             UnityExe = unityExe;
 
-            if(checkMatches) {
+            if (checkMatches) {
                 BestMatches(exeBytes);
                 return;
             }
 
-            if(whiteBytes.Length != darkBytes.Length)
+            if (whiteBytes.Length != darkBytes.Length)
                 throw new Exception("Non matching number of skins hexes");
 
-            for(var i = 0; i < darkBytes.Length; i++) {
+            for (var i = 0; i < darkBytes.Length; i++) {
                 OffsetOfSkinFlags = GetOffsetOfSkinFlag(exeBytes, whiteBytes[i], darkBytes[i]);
 
-                if(OffsetOfSkinFlags != -1) {
+                if (OffsetOfSkinFlags != -1) {
                     SkinIndex = i;
                     break;
                 }
             }
 
-            if(OffsetOfSkinFlags != -1) {
+            if (OffsetOfSkinFlags != -1) {
                 IsDarkSkin = ArrayMatch(exeBytes, OffsetOfSkinFlags, darkBytes[SkinIndex]);
                 IsWhiteSkin = ArrayMatch(exeBytes, OffsetOfSkinFlags, whiteBytes[SkinIndex]);
-                Log("Current skin {0} (index {1}, offset 0x{2:X8})", IsDarkSkin ? "dark" : IsWhiteSkin ? "white" : "none", SkinIndex, OffsetOfSkinFlags);
-            }
-            else
+                //Log("Current skin {0} (index {1}, offset 0x{2:X8})", IsDarkSkin ? "dark" : IsWhiteSkin ? "white" : "none", SkinIndex, OffsetOfSkinFlags);
+            } else
                 Log("Invalid executable, skin bytes not found");
         }
 
@@ -68,15 +67,14 @@ namespace DarkSkin {
 
             try {
                 Log("Applying {0} skin...", enable ? "dark" : "white");
-                using(var stream = File.Open(UnityExe, FileMode.Open)) {
+                using (var stream = File.Open(UnityExe, FileMode.Open)) {
                     stream.Position = OffsetOfSkinFlags;
                     // stream.Position = 0x011E1FB0;
                     // SkinIndex = 3;
                     stream.Write(enable ? darkBytes[SkinIndex] : whiteBytes[SkinIndex], 0, (enable ? darkBytes[SkinIndex] : whiteBytes[SkinIndex]).Length);
                 }
-                Log("Success!");
-            }
-            catch(Exception e) {
+                //Log("Success!");
+            } catch (Exception e) {
                 RestoreBackup();
                 throw e;
             }
@@ -84,7 +82,7 @@ namespace DarkSkin {
 
         private static byte[][] GetBytesFromHex(string[] hexes) {
             var results = new byte[hexes.Length][];
-            for(var i = 0; i < hexes.Length; i++) {
+            for (var i = 0; i < hexes.Length; i++) {
                 results[i] = GetBytesFromHex(hexes[i]);
             }
             return results;
@@ -95,19 +93,19 @@ namespace DarkSkin {
         }
 
         private static bool ArrayMatch(byte[] array, int offset, byte[] check) {
-            for(int i = 0, j = offset; i < check.Length; i++, j++)
-                if(j >= array.Length || j < 0 || array[j] != check[i])
+            for (int i = 0, j = offset; i < check.Length; i++, j++)
+                if (j >= array.Length || j < 0 || array[j] != check[i])
                     return false;
 
             return true;
         }
 
         private static void BestMatches(byte[] exeBytes) {
-            for(var i = 0; i < whiteBytes.Length; i++) {
+            for (var i = 0; i < whiteBytes.Length; i++) {
                 Console.WriteLine("\nTesting white skin sequence {0}", i);
                 BestMatches(exeBytes, whiteBytes[i]);
             }
-            for(var i = 0; i < darkBytes.Length; i++) {
+            for (var i = 0; i < darkBytes.Length; i++) {
                 Console.WriteLine("\nTesting dark skin sequence {0}", i);
                 BestMatches(exeBytes, darkBytes[i]);
             }
@@ -124,7 +122,7 @@ namespace DarkSkin {
             Console.BackgroundColor = ConsoleColor.Black;
             Parallel.For(c, c + 5000 /*exeBytes.Length*/, (i) => {
                 var match = MatchPercent(exeBytes, i, skinBytes);
-                if(match > 0.4)
+                if (match > 0.4)
                     Console.WriteLine("0x{0:X8} ({1:00.0%}):\t{2} - {3:0.00%}", i, (float)i / exeBytes.Length, FormatBytes(exeBytes, i, skinBytes.Length), match);
             });
         }
@@ -132,22 +130,22 @@ namespace DarkSkin {
         private static float MatchPercent(byte[] exeBytes, int offset, byte[] skinBytes) {
             var matches = 0;
 
-            for(int i = 0, j = offset; i < skinBytes.Length && j < exeBytes.Length; i++, j++)
-                if(exeBytes[j] == skinBytes[i])
+            for (int i = 0, j = offset; i < skinBytes.Length && j < exeBytes.Length; i++, j++)
+                if (exeBytes[j] == skinBytes[i])
                     matches++;
 
             return (float)matches / skinBytes.Length;
         }
 
         private static int GetOffsetOfSkinFlag(byte[] exeBytes, byte[] expectedWhiteSequence, byte[] expectedDarkSequence) {
-            if(expectedWhiteSequence.Length != expectedDarkSequence.Length)
+            if (expectedWhiteSequence.Length != expectedDarkSequence.Length)
                 throw new InvalidOperationException("Dark and white skin hex lengh don't match");
 
             var result = -1;
 
             Parallel.For(0, exeBytes.Length - expectedDarkSequence.Length, (addr, state) => {
-                for(var j = 0; j < expectedWhiteSequence.Length; j++)
-                    if(exeBytes[addr + j] != expectedWhiteSequence[j] && exeBytes[addr + j] != expectedDarkSequence[j])
+                for (var j = 0; j < expectedWhiteSequence.Length; j++)
+                    if (exeBytes[addr + j] != expectedWhiteSequence[j] && exeBytes[addr + j] != expectedDarkSequence[j])
                         return; // Our byte is different, continue to next sequence.
 
                 result = addr; // We matched all the bytes, return the address. 
@@ -158,16 +156,16 @@ namespace DarkSkin {
         }
 
         private void EnsureBackup() {
-            if(File.Exists(UnityBackupExe))
+            if (File.Exists(UnityBackupExe))
                 return;
 
-            Log("Backing up...", UnityExe);
+            // Log("Backing up...", UnityExe);
             var bytes = File.ReadAllBytes(UnityExe);
             File.WriteAllBytes(UnityBackupExe, bytes);
         }
 
         private void RestoreBackup() {
-            if(!File.Exists(UnityExe) || !File.Exists(UnityBackupExe))
+            if (!File.Exists(UnityExe) || !File.Exists(UnityBackupExe))
                 return;
 
             Log("Restoring backup...");
